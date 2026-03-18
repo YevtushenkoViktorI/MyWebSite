210c949d750953cd5475385afefef86394d0f48a
Viktor Yevtushenko <>
2026-03-18 21:23:11 +0100

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
+const myRepoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+const myIsUserPagesRepo = myRepoName.endsWith(".github.io");
+const myBasePath =
+  process.env.VITE_BASE_PATH ??
+  (process.env.GITHUB_ACTIONS ? (myIsUserPagesRepo ? "/" : `/${myRepoName}/`) : "/");
+
 export default defineConfig({
+  base: myBasePath,
   plugins: [react()],
   server: {
     host: true,
