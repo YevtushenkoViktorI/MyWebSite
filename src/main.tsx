b877d785aa980b58ad7792316753689cfa0d1b01
Viktor Yevtushenko <>
2026-04-06 22:12:16 +0200

    Added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,22 @@ import "./styles/colorPalette.css";
 import "./styles/localFonts.css";
 import "./styles/globalStyles.css";
 
+const myGaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
+
+if (myGaMeasurementId) {
+  const myScript = document.createElement("script");
+  myScript.async = true;
+  myScript.src = `https://www.googletagmanager.com/gtag/js?id=${myGaMeasurementId}`;
+  document.head.appendChild(myScript);
+
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function gtag(...myArgs: unknown[]) {
+    window.dataLayer!.push(myArgs);
+  };
+  window.gtag("js", new Date());
+  window.gtag("config", myGaMeasurementId);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MyApp />
